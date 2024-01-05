5105a2d466652f042e814425ff9f3cd43ab0cf83
keshavkumar526 <kj9646331@gmail.com>
2024-01-05 09:56:30 +0530

    readme_updated

--- frontend/src/frontEndConfig.js
+++ frontend/src/frontEndConfig.js
@@ -1,6 +1,5 @@
 const config = {
   SERVER_DOMAIN: "https://product-backend-1gdg.onrender.com",
-  // SERVER_DOMAIN: "https://backend-cityshop-fyxs.onrender.com/",
   STRIPE_PUBLIC_KEY:
     "pk_test_51OUR0BSDidszZ9POAulKBfnfxJmZCGORUjfIwzUbLs58jwRu3NkC1YyDkWlCNiUKD2Crqxsl3u85TKnDQ5GTLqpp00Nn9SEEce",
 };
